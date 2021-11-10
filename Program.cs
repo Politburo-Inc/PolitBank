4df7a8f554beff56d090cdda6e02f90e376f2495
Senecm <58762403+Senecm@users.noreply.github.com>
2021-11-10 21:20:24 +1030

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace PolitBank
+{
+    public class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to PolitBank!");
+            Console.WriteLine("");
+            loginMenu();
+        }
+
+        static void loginMenu()
+        {
+            Console.WriteLine("\n\nSelect Option: ");
+            Console.WriteLine("0. Quit");
+            Console.WriteLine("1. Create Account");
+            Console.WriteLine("2. Login");
+
+            string userInput = Console.ReadLine();
+            int processedInput = -1;
+
+            Validator.validate(userInput, processedInput, loginMenu);
+
+            switch (processedInput)
+            {
+                case 0:
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                default:
+                    Console.WriteLine("ERROR: NO VALID OPTION");
+                    break;
+            }
+        }
+
+        static void createAccount()
+        {
+            Console.WriteLine("Lets get you set up...");
+            Console.WriteLine("\n\nEnter Name: ");
+            string username = Console.ReadLine();
+            Console.WriteLine("\nEnter Age: ");
+            string userAge = Console.ReadLine();
+            int processedAge;
+            try
+            {  
+                processedAge = Convert.ToInt32(userAge);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("ERROR: Input failed!");
+                loginMenu();
+            }
+        }
+        static void login()
+        {
+            Console.WriteLine("");
+        }
+
+        static void menu()
+        {
+            Console.WriteLine("Select Option: ");
+            Console.WriteLine("1. Create Account");
+        }
+    }
+
+}
---
+++ Validator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolitBank
+{
+    internal static class Validator
+    {
+        public static int validate(string userInput, int processedInput, Action encapsulatedFunction)
+        {
+            try
+            {
+                processedInput = Convert.ToInt32(userInput);
+                return processedInput;
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("ERROR: INPUT FAILURE");
+                encapsulatedFunction();
+                return -1;
+            }
+        }
+    }
+}
